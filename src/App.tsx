6d45b4e4895c7c26fe905622c73e4bca25a0dd65
Priam Jain <41510589+priamjain@users.noreply.github.com>
2021-04-14 18:35:30 +0530

    logo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div>
-        
+        <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+
+
+function Navbar(): ReactElement {
+    return (
+        <div className="d-flex container">
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
