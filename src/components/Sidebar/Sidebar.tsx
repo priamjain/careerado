bb3af7ef3162898518b997b8aec64f3c76242456
Priam Jain <41510589+priamjain@users.noreply.github.com>
2021-04-19 23:25:00 +0530

    box-shadow changed

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,19 +10,19 @@ export const Sidebar = (props: Props) => {
         <div className={styles.main}>
             <p>Most Visited</p>
             <ul className={"mt-3 "+styles.list}>
-                <li className={"border border-dark rounded mt-2 text-center " + styles.card}>
+                <li className={"rounded mt-2 text-center " + styles.card}>
                     <Link to="/roadmap/react" className="p-3 no_style_link d-block w-100 h-100">
                         React
                     </Link>
                 </li>
 
-                <li className={"border border-dark rounded mt-2 text-center " + styles.card}>
+                <li className={"rounded mt-2 text-center " + styles.card}>
                     <Link to="/roadmap/backend" className="p-3 no_style_link d-block w-100 h-100">
                         Backend Development
                     </Link>
                 </li>
 
-                <li className={"border border-dark rounded mt-2 text-center " + styles.card}>
+                <li className={"rounded mt-2 text-center " + styles.card}>
                     <Link to="/roadmap/datascience" className="p-3 no_style_link d-block w-100 h-100">
                         Data Science
                     </Link>
@@ -30,13 +30,13 @@ export const Sidebar = (props: Props) => {
             </ul>
             <p className="mt-4">Coming Soon</p>
             <ul className={"mt-3 "+styles.list}>
-                <li className={"border border-dark rounded mt-2 text-center " + styles.card}>
+                <li className={"rounded mt-2 text-center " + styles.card}>
                     <Link to="#" className="p-3 no_style_link d-block w-100 h-100">
                         Devops
                     </Link>
                 </li>
 
-                <li className={"border border-dark rounded mt-2 text-center " + styles.card}>
+                <li className={"rounded mt-2 text-center " + styles.card}>
                     <Link to="#" className="p-3 no_style_link d-block w-100 h-100">
                         Frontend Development
                     </Link>
